7f99bda8690aa3b6b09d7e06e9e0a46561f5b2de
asumface <asumface@gmail.com>
2019-10-12 01:40:47 +0200

    another solution to day 1

--- source/day1/main.ml
+++ source/day1/main.ml
@@ -1,6 +1,13 @@
 open Core
 
 let lines = In_channel.read_lines "../../views/day1_input"
+
+(* solution 1 *)
 let nums = List.map ~f:(int_of_string) lines
 let result = List.fold ~f:(+) nums ~init:0
-let () = printf "%d" result
+let () = printf "%d\n" result
+
+(* solution 2 *)
+let () = List.map ~f:(int_of_string) lines
+    |> List.fold ~f:(+) ~init:0
+    |> printf "%d\n"
