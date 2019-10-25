2308ab4b7d7caee2c628b3a54e5f2fb05a03964d
asumface <asumface@gmail.com>
2019-10-26 00:58:31 +0200

    working solution to day 2, part 2 (PITA)

--- source/day2_part2/main.ml
+++ source/day2_part2/main.ml
@@ -19,60 +19,82 @@ let sequence_of_bigarray barr =
             else
                 None)
 
+let print_charseq seq =
+    Sequence.iter seq ~f:(printf "%c")
+
 let sequence_to_eol seq =
     Sequence.take_while ~f:(fun c -> match c with '\n' -> false | _ -> true) seq
 
+let yes _ = true
+
+let cnt = ref 0
+
+let rec drop_to_nextline seq =
+    let rec drop_newlines seq =
+        let next = Sequence.next seq in
+        match next with
+        | None -> Sequence.drop_eagerly seq 1
+        | Some (head, tail) -> match head with
+            | '\n' -> drop_newlines tail
+            | _ -> seq
+            in
+    let next = Sequence.next seq in
+    match next with
+    | None -> Sequence.drop_eagerly seq 1
+    | Some (head, tail) -> match head with
+        | '\n' -> drop_newlines seq
+        | _ -> drop_to_nextline tail
+
 let sequence_lines barr =
     let sob = sequence_of_bigarray barr in
-    let empty = sequence_to_eol (Sequence.of_list ['\n']) in
-    Sequence.unfold ~init:(empty, sob)
-        ~f:(fun (prev, rem) ->
+    Sequence.unfold ~init:sob
+        ~f:(fun (rem) ->
             match Sequence.next rem with
             | Some _ ->
-                let rem = Sequence.drop_eagerly rem (Sequence.count prev ~f:(fun _ -> true)) in
-                Some (sequence_to_eol rem, (sequence_to_eol rem, rem))
+                let rem = drop_to_nextline rem in
+                Some (sequence_to_eol rem, rem)
             | None -> None)
 
 let get_match str1 str2 =
-    let () = assert ((Sequence.count ~f:(fun _ -> true) str1) = (Sequence.count ~f:(fun _ -> true) str2)) in
+    let () = assert ((Sequence.count ~f:yes str1) = (Sequence.count ~f:yes str2)) in
     let overlap = Sequence.zip str1 str2 |> Sequence.filter_map ~f:(fun (a, b) -> if a = b then Some a else None) in
-    ((Sequence.count ~f:(fun _ -> true) str1) - (Sequence.count ~f:(fun _ -> true) overlap), overlap)
+    ((Sequence.count ~f:yes str1) - (Sequence.count ~f:yes overlap), overlap)
 
-let comp_thereafter ~comp_fun seq =
-    printf "%d " (Sequence.count ~f:(fun _ -> true) seq);
+let comp_thereafter ~comp_fun ~filter seq =
     let first, seq = Option.value_exn (Sequence.next seq) in
+    let () = assert (Sequence.count ~f:yes first |> (=) 26) in
     Sequence.unfold ~init:(first, seq)
-        ~f:(fun (lhs, remainder) ->
-            if (Sequence.count ~f:(fun _ -> true) seq) > 1 then
+        ~f:(let rec rg (lhs, remainder) =
+            if (Sequence.count ~f:yes remainder) > 1 then
                 let rhs, remainder = Option.value_exn (Sequence.next remainder) in
-                Some (comp_fun lhs rhs, (first, remainder))
+                if filter rhs then
+                    Some (comp_fun lhs rhs, (first, remainder))
+                else
+                    rg (first, remainder)
             else
-                None)
+                None
+            in rg)
 
 let drop_sequence ~min seq =
     Sequence.unfold ~init:seq
         ~f:(fun seq ->
-            printf "%d " (Sequence.count ~f:(fun _ -> true) seq);
-            printf "%d " (Sequence.count ~f:(fun _ -> true) seq);
             let _, remainder = Option.value_exn (Sequence.next seq) in
-            printf "%d " (Sequence.count ~f:(fun _ -> true) remainder);
-            if Sequence.count ~f:(fun _ -> true) seq >= min then
+            if Sequence.count ~f:yes seq >= min then
                 Some (seq, remainder)
             else
                 None)
 
-let print_charseq seq =
-    Sequence.iter seq ~f:(printf "%c")
-
 let box_ids = sequence_lines input_file
-
 let find_solution seq =
     Sequence.iter
         (drop_sequence ~min:2 seq
-            |> Sequence.map ~f:(comp_thereafter ~comp_fun:get_match)
+            |> Sequence.map
+                ~f:(comp_thereafter
+                    ~comp_fun:get_match
+                    ~filter:(fun rhs -> Sequence.bounded_length rhs ~at_most:0 = `Greater))
             |> Sequence.concat)
         ~f:(fun m -> match m with
-            | (2, r) -> print_charseq r
+            | (1, r) -> print_charseq r; printf "\n"
             | _ -> ())
 
 let () = find_solution box_ids
